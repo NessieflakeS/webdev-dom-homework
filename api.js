99f57b41900b7d9eb16ec3c23f74a2f49dbe6d9b
NessieflakeS <nikandrof.danil@gmail.com>
2025-08-05 19:53:06 +0800

    fix URL

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = "https:// 6891c1a2447ff4f11fbd9fda.mockapi.io/comments";
+const API_URL = "https://6891c1a2447ff4f11fbd9fda.mockapi.io/comments/:endpoint";
 
 const handleApiError = (response) => {
   if (response.status === 404) throw new Error("Ресурс не найден");
